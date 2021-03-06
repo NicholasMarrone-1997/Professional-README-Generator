32e8c65daed1aae4826ee5f416d7126e938af6dd
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-06 12:16:48 -0500

    Built out generateMarkdown function as well as questions and literals, trying to figure out how to write to file.

--- index.js
+++ index.js
@@ -1,14 +1,94 @@
 // TODO: Include packages needed for this application
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+
+const generateReadme = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the project title',
+        name: 'project',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is the project description',
+        name: 'description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'What are the install instructions?',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'What are the usage instructions?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What License can you use?',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Who is Contributing?',
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        message: 'What is your link to your github?',
+        name: 'questions',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
+            return true;
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+     .prompt(questions)
+     .then(answers =>{
+         var title = answers.project;
+         writeToFile(title, answers);
+        
 
+    });
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+  https://github.com/${data.questions}/${data.project}
 
+  ## Table of Contents
+  [Description](#description)
+  [Install](#install)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contribute)
+  [Tests](#test)
+  [Questions](#questions)
+
+  # Installation
+  The following must be installed in order for this application to run: ${data.install}
+
+  # Usage
+  To use this application: ${data.usage}
+
+  # Contributing 
+  Contributors: ${data.contribute}
+
+  # Test Instructions
+  The following is needed to run the application test: ${data.test}
+
+  # Github Username and Link
+  Input github username and repo: ${data.name}
 `;
 }
 
